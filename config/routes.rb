05d4e03448dea5386254fd5743cf727d7f7285fe
Ben Craven <ben.craven@deliveroo.co.uk>
2017-12-28 14:53:18 +0000

    Fill in basic layout and create route to user signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
-  get 'static_pages/contact'
 
+  root 'static_pages#home'
+  get  '/help', to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup', to: 'users#new'
+
+  # allows for custom get routes. e.g. /signup creates signup_path
+  # which directs the browser to users/new but displays /signup in the browser
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
